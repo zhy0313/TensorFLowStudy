43f8614ace71f9c9c53327612e8bd2e92614f1a4
Bo <chaibo24@outlook.com>
2017-04-19 16:25:01 +0800

    add xavier_init function

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -3,3 +3,8 @@ import sklearn.preprocessing as prep
 import tensorflow as tf 
 from tensorflow.examples.tutorials.mnist import input_data
 
+def xavier_init(fan_in,fan_out,constant=1):
+    low=-constant*np.sqrt(6.0/(fan_in+fan_out))
+    high=constant*np.sqrt(6.0/(fan_in+fan_out))
+    return tf.random_uniform((fan_in,fan_out),minval=low,maxval=high,dtype=tf.float32)
+
