f076d93063f210561e7d708f3c4fcb459f9b58dc
Bo <chaibo24@outlook.com>
2017-04-25 16:30:16 +0800

    add some annotation

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -9,20 +9,22 @@ def xavier_init(fan_in,fan_out,constant=1):
     high=constant*np.sqrt(6.0/(fan_in+fan_out))
     return tf.random_uniform((fan_in,fan_out),minval=low,maxval=high,dtype=tf.float32)
 
+
 class AdditiveGaussianNoiseAutoencoder(object):
     def __init__(self,n_input,n_hidden,transfer_function=tf.nn.softplus,optimizer=tf.train.AdamOptimizer(),scale=0.1):
-        self.n_input=n_input
-        self.n_hidden=n_hidden
-        self.transfer=transfer_function
+        self.n_input=n_input       #输入变量数
+        self.n_hidden=n_hidden     #隐含层节点数
+        self.transfer=transfer_function      #隐含层激活函数
         self.scale=tf.placeholder(tf.float32)
-        self.training_scale=scale
+        self.training_scale=scale            #高斯噪声系数
         network_weights=self._initialize_weights()
         self.weights=network_weights
 
         self.x=tf.placeholder(tf.float32,[None,self.n_input])
+        #隐含层
         self.hidden=self.transfer(tf.add(tf.matmul(self.x+scale*tf.random_uniform((n_input,)),self.weights['w1']),self.weights['b1']))
+        #输出层数据复原、重建
         self.reconstruction=tf.add(tf.matmul(self.hidden,self.weights['w2']),self.weights['b2'])
-
         self.cost=0.5*tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction,self.x),2.0))
         self.optimizer=optimizer.minimize(self.cost)
         init=tf.global_variables_initializer()
