6a7d707dcf833d21ab9d379b67643db9fda847ab
Bo <chaibo24@outlook.com>
2017-05-27 15:22:08 +0800

    finish GoogleInceptionNet_V3.py

--- GoogleInceptionNet_V3.py
+++ GoogleInceptionNet_V3.py
@@ -1,5 +1,8 @@
 #使用TensorFlow实现Google Inception Net V3
 
+from datetime import datetime
+import time
+import math
 import tensorflow as tf
 slim=tf.contrib.slim
 trunc_normal=lambda stddev:tf.truncated_normal_initializer(0.0,stddev)
@@ -15,7 +18,7 @@ def inception_v3_arg_scope(weight_decay=0.00004,stddev=0.1,batch_norm_var_collec
             'beta':None,
             'gamma':None,
             'moving_mean':[batch_norm_var_collection],
-            'moving_variance':[batch_norm_var_collectio],
+            'moving_variance':[batch_norm_var_collection],
         }
     }
 
@@ -31,8 +34,8 @@ def inception_v3_arg_scope(weight_decay=0.00004,stddev=0.1,batch_norm_var_collec
 def inception_v3_base(inputs,scope=None):
     """
     :param inputs:输入图片数据的tensor 
-    :param scope: 包含函数默认参数的环境
-    :return: 
+    :param scope:包含函数默认参数的环境
+    :return:卷积输出及重要节点的字典表
     """
 
     end_points={}
@@ -242,14 +245,14 @@ def inception_v3_base(inputs,scope=None):
                     branch_0=slim.conv2d(net,320,[1,1],scope='Conv2d_0a_1x1')
 
                 with tf.variable_scope('Branch_1'):
-                    branch_1=slim.conv2d(net,384,[1,1],scope='Conv2d_0a,1x1')
+                    branch_1=slim.conv2d(net,384,[1,1],scope='Conv2d_0a_1x1')
                     branch_1=tf.concat([
                         slim.conv2d(branch_1,384,[1,3],scope='Conv2d_0b_1x3'),
                         slim.conv2d(branch_1,384,[3,1],scope='Conv2d_0c_3x1')],3)
 
                 with tf.variable_scope('Branch_2'):
-                    branch_2=slim.conv2d(net,488,[1,1],scope='Conv2d_0a,1x1')
-                    branch_2=slim.conv2d(branch_2,384,[3,3],scope='Conv2d_0b,3x3')
+                    branch_2=slim.conv2d(net,488,[1,1],scope='Conv2d_0a_1x1')
+                    branch_2=slim.conv2d(branch_2,384,[3,3],scope='Conv2d_0b_3x3')
                     branch_2=tf.concat([
                         slim.conv2d(branch_2,384,[1,3],scope='Conv2d_0c_1x3'),
                         slim.conv2d(branch_2,384,[3,1],scope='Conv2d_0d_3x1')],3)
@@ -266,14 +269,14 @@ def inception_v3_base(inputs,scope=None):
                     branch_0 = slim.conv2d(net, 320, [1, 1], scope='Conv2d_0a_1x1')
 
                 with tf.variable_scope('Branch_1'):
-                    branch_1 = slim.conv2d(net, 384, [1, 1], scope='Conv2d_0a,1x1')
+                    branch_1 = slim.conv2d(net, 384, [1, 1], scope='Conv2d_0a_1x1')
                     branch_1 = tf.concat([
                         slim.conv2d(branch_1, 384, [1, 3], scope='Conv2d_0b_1x3'),
                         slim.conv2d(branch_1, 384, [3, 1], scope='Conv2d_0c_3x1')], 3)
 
                 with tf.variable_scope('Branch_2'):
-                    branch_2 = slim.conv2d(net, 488, [1, 1], scope='Conv2d_0a,1x1')
-                    branch_2 = slim.conv2d(branch_2, 384, [3, 3], scope='Conv2d_0b,3x3')
+                    branch_2 = slim.conv2d(net, 488, [1, 1], scope='Conv2d_0a_1x1')
+                    branch_2 = slim.conv2d(branch_2, 384, [3, 3], scope='Conv2d_0b_3x3')
                     branch_2 = tf.concat([
                         slim.conv2d(branch_2, 384, [1, 3], scope='Conv2d_0c_1x3'),
                         slim.conv2d(branch_2, 384, [3, 1], scope='Conv2d_0d_3x1')], 3)
@@ -286,11 +289,80 @@ def inception_v3_base(inputs,scope=None):
 
     return net,end_points
 
+def inception_v3(inputs,num_classes=1000,is_training=True,dropout_keep_prob=0.8,prediction_fn=slim.softmax,
+                 spatial_squeeze=True,reuse=None,scope='InceptionV3'):
+    """
+    :param inputs:输入图片数据的tensor
+    :param num_classes:需要分类的数量
+    :param is_training:标志是否是训练过程
+    :param dropout_keep_prob:Dropout比例
+    :param prediction_fn:分类函数
+    :param spatial_squeeze:是否对输出进行squeeze操作
+    :param reuse:标志是否会对网络和Variable进行重复使用
+    :param scope:包含函数默认参数的环境
+    :return:
+    """
 
+    with tf.variable_scope(scope,'InceptionV3',[inputs,num_classes],reuse=reuse) as scope:
+        with slim.arg_scope([slim.batch_norm,slim.dropout],is_training=is_training):
+            net,end_points=inception_v3_base(inputs,scope=scope)
 
+            with slim.arg_scope([slim.conv2d,slim.max_pool2d,slim.avg_pool2d],stride=1,padding='SAME'):
+                aux_logits=end_points['Mixed_6e']
+                with tf.variable_scope('AuxLogits'):
+                    aux_logits=slim.avg_pool2d(aux_logits,[5,5],stride=3,padding='VALID',scope='AvgPool_1a_5x5')
+                    aux_logits=slim.conv2d(aux_logits,128,[1,1],scope='Conv2d_1b_1x1')
+                    aux_logits=slim.conv2d(aux_logits,768,[5,5],weights_initializer=trunc_normal(0.01),
+                                           padding='VALID',scope='Conv2d_2a_5x5')
+                    aux_logits=slim.conv2d(aux_logits,num_classes,[1,1],activation_fn=None,normalizer_fn=None,
+                                           weights_initializer=trunc_normal(0.001),scope='Conv2d_2b_1x1')
+                    if spatial_squeeze:
+                        aux_logits=tf.squeeze(aux_logits,[1,2],name='SpatialSqueeze')
+                    end_points['AuxLogits']=aux_logits
 
+                with tf.variable_scope('Logits'):
+                    net=slim.avg_pool2d(net,[8,8],padding='VALID',scope='AvgPool_1a_8x8')
+                    net=slim.dropout(net,keep_prob=dropout_keep_prob,scope='Dropout_1b')
+                    end_points['PreLogits']=net
+                    logits=slim.conv2d(net,num_classes,[1,1],activation_fn=None,normalizer_fn=None,scope='Conv2d_1c_1x1')
+                    if spatial_squeeze:
+                        logits=tf.squeeze(logits,[1,2],name='SpatialSqueeze')
 
+                    end_points['Logits']=logits
+                    end_points['Predictions']=prediction_fn(logits,scope='Predictions')
 
+    return logits,end_points
 
+#评估每轮计算时间的函数
+def time_tensorflow_run(session,target,info_string):
+    num_steps_burn_in=10       #预热轮数
+    total_duration=0.0
+    total_duration_squared=0.0
 
+    for i in range(num_batches+num_steps_burn_in):
+        start_time=time.time()
+        _=session.run(target)
+        duration=time.time()-start_time
+        if i>=num_steps_burn_in:
+            if not i%10:
+                print('%s:step %d,duration=%.3f' % (datetime.now(),i-num_steps_burn_in,duration))
+            total_duration+=duration
+            total_duration_squared+=duration*duration
 
+    mn=total_duration/num_batches   #平均耗时
+    vr=total_duration_squared/num_batches-mn*mn
+    sd=math.sqrt(vr)   #标准差
+
+    print('%s:%s across %d steps,%.3f +/- %.3f sec/batch' % (datetime.now(),info_string,num_batches,mn,sd))
+
+batch_size=32
+height,width=299,299
+inputs=tf.random_uniform((batch_size,height,width,3))
+with slim.arg_scope(inception_v3_arg_scope()):
+    logits,end_points=inception_v3(inputs,is_training=True)
+
+init=tf.global_variables_initializer()
+sess=tf.Session()
+sess.run(init)
+num_batches=100
+time_tensorflow_run(sess,logits,"Forward")
