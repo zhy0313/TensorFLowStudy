3480b0a5e9541eea070b76a6c44430d66dfbb5f0
Bo <chaibo24@outlook.com>
2017-06-13 17:30:01 +0800

    modify Word2Vec.py

--- Word2Vec.py
+++ Word2Vec.py
@@ -121,3 +121,65 @@ valid_window=100  #验证单词只从频数最高的100个单词中抽取
 valid_examples=np.random.choice(valid_window,valid_size,replace=False)  #抽取验证单词
 num_sampled=64   #噪声单词数量
 
+graph=tf.Graph()
+with graph.as_default():
+    train_inputs=tf.placeholder(tf.int32,shape=[batch_size])
+    train_labels=tf.placeholder(tf.int32,shape=[batch_size,1])
+    valid_dataset=tf.constant(valid_examples,dtype=tf.int32)
+
+    with tf.device('/cpu:0'):
+        embeddings=tf.Variable(tf.random_uniform([vocabulary_size,embedding_size],-1.0,1.0))
+        embed=tf.nn.embedding_lookup(embeddings,train_inputs)
+
+        nce_weights=tf.Variable(tf.truncated_normal([vocabulary_size,embedding_size],stddev=1.0/math.sqrt(embedding_size)))
+        nce_biases=tf.Variable(tf.zeros([vocabulary_size]))
+
+    loss=tf.reduce_mean(tf.nn.nce_loss(weights=nce_weights,
+                                       biases=nce_biases,
+                                       labels=train_labels,
+                                       inputs=embed,
+                                       num_sampled=num_sampled,
+                                       num_classes=vocabulary_size))
+
+    optimizer=tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+
+    norm=tf.sqrt(tf.reduce_sum(tf.square(embeddings),1,keep_dims=True))
+    normalized_embeddings=embeddings/norm
+    valid_embeddings=tf.nn.embedding_lookup(normalized_embeddings,valid_dataset)
+    similarity=tf.matmul(valid_embeddings,normalized_embeddings,transpose_b=True)
+
+    init=tf.global_variables_initializer()
+
+    num_steps=100001
+
+    with tf.Session(graph=graph) as session:
+        init.run()
+        print('Initialized')
+
+        average_loss=0
+        for step in range(num_steps):
+            batch_inputs,batch_labels=generate_batch(batch_size,num_skips,skip_window)
+            feed_dict={train_inputs:batch_inputs,train_labels:batch_labels}
+
+            _,loss_val=session.run([optimizer,loss],feed_dict=feed_dict)
+            average_loss+=loss_val
+
+            if step%2000==0:
+                if step>0:
+                    average_loss/=2000
+                print("Average loss at step ",step,":",average_loss)
+                average_loss=0
+
+            if step%10000==0:
+                sim=similarity.eval()
+                for i in range(valid_size):
+                    valid_word=reverse_dictionary[valid_examples[i]]
+                    top_k=8
+                    nearest=(-sim[i,:]).argsort()[1:top_k+1]
+                    log_str="Nearst to %s:" %valid_word
+                    for k in range(top_k):
+                        close_word=reverse_dictionary[nearest[k]]
+                        log_str="%s %s,"%(log_str,close_word)
+                    print(log_str)
+
+        final_embeddings=normalized_embeddings.eval()
