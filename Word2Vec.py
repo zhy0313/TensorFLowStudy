936493bf93cbebd62b992a3a0ed267fcc68a9c25
Bo <chaibo24@outlook.com>
2017-06-14 17:05:08 +0800

    modify Word2Vec.py

--- Word2Vec.py
+++ Word2Vec.py
@@ -183,3 +183,20 @@ with graph.as_default():
                     print(log_str)
 
         final_embeddings=normalized_embeddings.eval()
+
+def plot_with_labels(low_dim_embs,labels,filename='tsne.png'):
+    assert low_dim_embs.shape[0]>=len(labels),"More labels than embeddings"
+    plt.figure(figsize=(18,18))
+    for i,label in enumerate(labels):
+        x,y=low_dim_embs[i,:]
+        plt.scatter(x,y)
+        plt.annotate(label,xy=(x,y),xytext=(5,2),textcoords='offset points',ha='right',va='bottom')
+    plt.savefig(filename)
+
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+tsne=TSNE(perplexity=30,n_components=2,init='pca',n_iter=5000)
+plot_only=1000
+low_dim_embs=tsne.fit_transform(final_embeddings[:plot_only,:])
+labels=[reverse_dictionary[i] for i in range(plot_only)]
+plot_with_labels(low_dim_embs,labels)
