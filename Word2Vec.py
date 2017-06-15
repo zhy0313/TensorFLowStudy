58bd89d1ea52ee71b50d7c27e204fb2283f464d7
Bo <chaibo24@outlook.com>
2017-06-15 16:17:43 +0800

    modify Word2Vec.py

--- Word2Vec.py
+++ Word2Vec.py
@@ -196,7 +196,7 @@ def plot_with_labels(low_dim_embs,labels,filename='tsne.png'):
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 tsne=TSNE(perplexity=30,n_components=2,init='pca',n_iter=5000)
-plot_only=1000
+plot_only=100
 low_dim_embs=tsne.fit_transform(final_embeddings[:plot_only,:])
 labels=[reverse_dictionary[i] for i in range(plot_only)]
 plot_with_labels(low_dim_embs,labels)
